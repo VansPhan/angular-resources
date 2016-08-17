2a8d5dac9ae161924144b5d81a0b4b07e6f3d1e5
Van Phan <VanPhan514@gmail.com>
2016-08-17 09:05:17 -0400

    Switching branch

--- js/grumbles/grumble.factory.js
+++ js/grumbles/grumble.factory.js
@@ -3,12 +3,11 @@
 		.module("grumbles")
 		.factory("GrumbleFactory", [
 			"$resource",
+			"$http",
 			GrumbleFactoryFunction
 		]);
 
-		function GrumbleFactoryFunction($resource) {
-			return $resource("http://localhost:3000/grumbles/:id", {}, {
-				update: { method: "PUT" }
-			});
+		function GrumbleFactoryFunction($resource, $http) {
+			return $http("https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/21418698?api_key=d4095fd1-ec30-4403-8db7-fdacebef9b17");
 		}
 }());
